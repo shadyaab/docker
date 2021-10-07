703e7a6a1afdbf2769017ed8fa8c4b6e71eb4581
shadyaab <shadyaab543@gmail.com>
2021-10-07 23:49:10 +0530

    Initial commit

---
+++ src/main/java/com/sady/docker/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.sady.docker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/demo")
+    public ResponseEntity<?> demo(){
+        return new ResponseEntity<>("sady", HttpStatus.OK);
+    }
+
+}
